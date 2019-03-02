cca220f5f2a880963462f79b15e3085893c735fc
corlick98 <corlick@ualberta.ca>
2019-03-02 15:20:08 -0700

    Added changing pages

--- q2.py
+++ q2.py
@@ -1,17 +1,42 @@
 def question2(connection):
     p_df = pd.read_sql_query("select * from papers;",connection)
-    print(p_df)
-    invalid=True
-    while invalid:
-        try:
-            paperid = int(input("select a paper ID: "))
-        except ValueError:
-            print("Please enter a valid paper ID")
-            continue
-        if (paperid-1) in p_df["Id"]:
-            invalid = False
-        else:
-            print("Please enter a valid paper ID")
+    notselected = True
+    start = 0
+    end = 5
+    while notselected:
+        print(p_df[start:end])
+        invalid=True
+        while invalid:
+            try:
+                paperid = input("select a paper ID: ")
+                if paperid == "N":
+                    if start+5<len(p_df):
+                        start+=5
+                    if end+5<len(p_df):
+                        end+=5
+                    else:
+                        end = len(p_df)
+                    break
+                elif paperid == "P":
+                    if end-5>0:
+                        end-=5
+                    if start-5>0:
+                        start-=5
+                    else:
+                        start=0
+                    if end-start<5:
+                        end=start+5
+                    break
+                else:
+                    paperid = int(paperid)
+            except ValueError:
+                print("Please enter a valid paper ID")
+                continue
+            if (paperid-1) in p_df["Id"]:
+                invalid = False
+                notselected = False
+            else:
+                print("Please enter a valid paper ID")
     c = connection.cursor()
     c.execute('''select e.reviewer 
                 from expertise e
