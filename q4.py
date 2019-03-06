0b5b5a0269a6ea07455596a6b35a3343174fd7dd
Fahad Ahammed <ahaammed@cs.ualberta.ca>
2019-03-05 23:03:02 -0700

    query 4 completed

--- q4.py
+++ q4.py
@@ -1,27 +1,32 @@
 
 def question4(connection):
     df = pd.read_sql_query("select author,count(csession) as count from papers group by author;", connection)
+    valid_input = False
+    
+    while valid_input == False:
+        try:
+            usr_input = int(input("\nChoose your option.\n1. bar plot of all individual authors and how many sessions they participate in \n2. providing a number for a selected individual\n"))
+        except ValueError:
+            print("\nPlease enter a valid selection\n")
+            time.sleep(0.5)
+            continue
+        
+        if usr_input not in range(1,3):
+            print("\nPlease enter a valid selection\n")
+            time.sleep(1)
+            continue
 
-
-    usr_input = int(input("\nChoose your option.\n1. bar plot of all individual authors and how many sessions they participate in \n2. providing a number for a selected individual\n"))
-
-    while usr_input not in range(1,3):
-        print('\nPlease enter a valid selection\n')
-        time.sleep(1)
-        continue
-
-    if usr_input == 1:
-        plot = df.plot.bar(x="author")
-        plt.plot()
-        plt.show()
-
-    elif usr_input == 2:
-        print(df.iloc[ : , 0])
-        author_name = (input("\nwrite the email of the author: "))
-        print()
-        print(df[df.author== author_name])
-        print()
-                       
+        if usr_input == 1:
+            plot = df.plot.bar(x="author")
+            plt.plot()
+            plt.show()
+            valid_input =  True
+            
+        elif usr_input == 2:
+            print(df.iloc[ : , 0])
+            author_name = (input("\nwrite the email of the author: "))
+            print(df[df.author== author_name])
+            valid_input = True           
         
                           
 
