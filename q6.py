f0a418273d8b6fe66c16991b6edf6cef794ec462
Bryland Schoneck <schoneck@cs.ualberta.ca>
2019-03-07 16:09:43 -0700

    q3,q6 comment

--- q6.py
+++ q6.py
@@ -1,13 +1,14 @@
+# Find the AVG of each column for each of reviewer
+# Display the results on a bar chart
+# A single grouped bar chart is shown with the reviewer as the x-axis label
 def question6(connection):
-    
-    
+    # Query and format a table showing the reviewer along with their AVG scores
     r_df = pd.read_sql_query("select reviewer, AVG(originality), AVG(importance), \
     AVG(soundness), AVG(overall) from reviews group by reviewer;",connection)
 
-    
-    # print(r_df)
+    # Chart formated table with data being all columns but the reviewer
     r_df2 = pd.DataFrame(r_df.iloc[:,1:], columns=r_df.columns[1:])
-    r_df2.plot.bar().set_xticklabels(r_df.iloc[:,0])
+    r_df2.plot.bar().set_xticklabels(r_df.iloc[:,0]) # Label x-axis using reviewer
     plt.plot()
     plt.show()
 
