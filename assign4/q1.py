23b35eada2b0fe703309d71311331411db4f35d6
corlick98 <corlick@ualberta.ca>
2019-03-23 14:21:18 -0600

    Q1 done

---
+++ assign4/q1.py
@@ -0,0 +1,25 @@
+
+
+def f1(conn):
+    start = input("Enter start year (YYYY): ")
+    end = input("Enter end year (YYYY): ")
+    ctype = input("Enter crime type: ")
+    df = pd.read_sql_query('''select Month, sum(Incidents_Count) as count
+                            from crime_incidents 
+                            where crime_type = ?
+                            and year between ? and ?
+                            group by month
+                            union 
+                            select Month,0
+                            from crime_incidents
+                            where Month!='Month'
+                            group by Month
+                            except 
+                            select Month, 0
+                            from crime_incidents 
+                            where crime_type = ?
+                            and year between ? and ?
+                            group by month;''',conn,params=(ctype, start,end,ctype, start,end))
+    plot = df.plot.bar(x='Month')
+    plt.plot()
+    plt.show()
