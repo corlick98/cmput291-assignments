8177ba97c72830d78220c3202120059e10e057b2
Bryland Schoneck <schoneck@cs.ualberta.ca>
2019-03-30 12:18:27 -0600

    Commenting bs

--- assign4/q2.py
+++ assign4/q2.py
@@ -2,7 +2,9 @@
 # Given an integer N, show (in a map) the N-most populous 
 # and N-least populous neighborhoods with their population count
 def f2(connection, q2count):
+    # Add to count number
     q2count += 1
+    
     Total_pop = 'CANADIAN_CITIZEN + NON_CANADIAN_CITIZEN + NO_RESPONSE'
 
     # Create query to show neighbourhoods and its coordinates and sort by population
@@ -15,17 +17,23 @@ def f2(connection, q2count):
                                 order by %s DESC;'''
                                 % (Total_pop, Total_pop, Total_pop),
                                 connection)
-    ###print(p_df.iloc[0,1])
+
     # Enter number of locations 
     num_local = int(input("Enter number of locations: "))
 
     # Generate centralized map of Edmonton
     q2map = folium.Map(location=[53.5444, -113.4909], zoom_start=12)
+
+    # Create max/min for relative sizes
     maxsize = p_df.iloc[0,3]
     minsize = p_df.iloc[-1,3]
+
+    # For loop for N locations top and bottom
     for N in range(num_local):
+        # Popup labels and format
         top_pop = str(p_df.iloc[N,0]) + ' <br> ' + str(p_df.iloc[N,3])
         bot_pop = str(p_df.iloc[-(N+1),0]) + ' <br> ' + str(p_df.iloc[-(N+1),3])
+        # Create circle for top N
         folium.Circle(
             location = [p_df.iloc[N,1], p_df.iloc[N,2]],
             popup = top_pop,
@@ -34,6 +42,7 @@ def f2(connection, q2count):
             fill = True,
             fill_color = 'crimson'
         ).add_to(q2map)
+        # Create circle for bottom N
         folium.Circle(
             location = [p_df.iloc[-(N+1),1], p_df.iloc[-(N+1),2]],
             popup = bot_pop,
@@ -42,8 +51,10 @@ def f2(connection, q2count):
             fill = True,
             fill_color = 'crimson'
         ).add_to(q2map)
-        
+    
+    # Save as new map 
     name = 'Q2-'+str(q2count)+'.html'
     q2map.save(name)
 
+    # Return count of maps
     return q2count
