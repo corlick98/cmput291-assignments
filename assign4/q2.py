d0feb017c941faea5c453c311be6dd28dbdf44c0
Bryland Schoneck <schoneck@cs.ualberta.ca>
2019-03-30 13:06:31 -0600

    ties are shit

--- assign4/q2.py
+++ assign4/q2.py
@@ -4,12 +4,15 @@
 def f2(connection, q2count):
     # Add to count number
     q2count += 1
-    
+
+    # Enter number of locations 
+    num_local = int(input("Enter number of locations: "))
+
     Total_pop = 'CANADIAN_CITIZEN + NON_CANADIAN_CITIZEN + NO_RESPONSE'
 
     # Create query to show neighbourhoods and its coordinates and sort by population
     # from the combination of the three population columns
-    p_df = pd.read_sql_query('''select p.Neighbourhood_Name, Latitude, Longitude, Sum(%s)
+    p_df = pd.read_sql_query('''select p.Neighbourhood_Name, Latitude, Longitude, Sum(%s) as Tpop
                                 from population p, coordinates c
                                 where p.Neighbourhood_Name = c.Neighbourhood_Name
                                 and (%s) > 0
@@ -18,40 +21,47 @@ def f2(connection, q2count):
                                 % (Total_pop, Total_pop, Total_pop),
                                 connection)
 
-    # Enter number of locations 
-    num_local = int(input("Enter number of locations: "))
-
+    t_df = p_df.nlargest(num_local, "Tpop", keep = "all")
+    b_df = p_df.nsmallest(num_local, "Tpop", keep = "all")
+    print(t_df)
+    print(b_df)
     # Generate centralized map of Edmonton
     q2map = folium.Map(location=[53.5444, -113.4909], zoom_start=11)
 
     # Create max/min for relative sizes
-    maxsize = p_df.iloc[0,3]
-    minsize = p_df.iloc[-1,3]
+    maxsize = t_df.iloc[0,3]
+    minsize = b_df.iloc[0,3]
 
-    # For loop for N locations top and bottom
-    for N in range(num_local):
+    # For loop for N locations top
+    for N in range(len(t_df)):
         # Popup labels and format
-        top_pop = str(p_df.iloc[N,0]) + ' <br> ' + str(p_df.iloc[N,3])
-        bot_pop = str(p_df.iloc[-(N+1),0]) + ' <br> ' + str(p_df.iloc[-(N+1),3])
+        top_pop = str(t_df.iloc[N,0]) + ' <br> ' + str(t_df.iloc[N,3])
+        
         # Create circle for top N
         folium.Circle(
-            location = [p_df.iloc[N,1], p_df.iloc[N,2]],
+            location = [t_df.iloc[N,1], t_df.iloc[N,2]],
             popup = top_pop,
-            radius = (1000 * p_df.iloc[N,3]) / maxsize,
+            radius = (1000 * t_df.iloc[N,3]) / maxsize,
             color = 'crimson',
             fill = True,
             fill_color = 'crimson'
         ).add_to(q2map)
+        
+    # For loop for N locations bottom
+    for N in range(len(b_df)):
+        # Popup labels and format
+        bot_pop = str(b_df.iloc[N,0]) + ' <br> ' + str(b_df.iloc[N,3])
+
         # Create circle for bottom N
         folium.Circle(
-            location = [p_df.iloc[-(N+1),1], p_df.iloc[-(N+1),2]],
+            location = [b_df.iloc[N,1], b_df.iloc[N,2]],
             popup = bot_pop,
-            radius = (100 * p_df.iloc[-(N+1),3]) / minsize,
+            radius = (100 * b_df.iloc[N,3]) / minsize,
             color = 'crimson',
             fill = True,
             fill_color = 'crimson'
         ).add_to(q2map)
-    
+
     # Save as new map 
     name = 'Q2-'+str(q2count)+'.html'
     q2map.save(name)
