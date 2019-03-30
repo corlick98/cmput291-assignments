100960c3c409b0c2af70e69acdf9f273a6ff74d9
Fahad Ahammed <ahaammed@cs.ualberta.ca>
2019-03-30 13:52:30 -0600

    got ridd of unnecessary comments

--- assign4/q3.py
+++ assign4/q3.py
@@ -1,7 +1,7 @@
 
 def f3(connection,q3count):
     invalid = True			# flag for checking the inputs
-    q3count += 1                    #forgot to count++
+    q3count += 1                    # count++
     while invalid:
         try:
             start = input("\nEnter start Year (YYYY):")		# start year in range
@@ -26,26 +26,23 @@ def f3(connection,q3count):
                 order by Count Desc;''' \
                 %(start, end, crime),connection)
     pdf = df.nlargest(N, 'Count', keep='all')			# the dataframe is created with the N largest tuples and ties are counted
-    #print(pdf)
-    maxval = pdf.iloc[0,1]
+    maxval = pdf.iloc[0,1]              # get the largest count for scaling
     m = folium.Map(location=[53.5444, -113.4909], zoom_start=11)             # declaring a folium. map variable with the co-ordinates
     counter = 0								# a counter to add each tuple to the map
-    #print(pdf.iloc[counter,1])
-    #print(type(pdf.iloc[counter,1]))
     while (counter < N):
         pop_up = str(pdf.iloc[counter,0]) + ' <br> ' + str(pdf.iloc[counter,1])		# declaring the pop_up the to be the name of the 
         									                                        # neighborhood and its crime count 
         folium.Circle(						# making the circle and giving its arguments
             location= [pdf.iloc[counter,2], pdf.iloc[counter,3]],		# location from the tuple
             popup = pop_up,							# popup is initialized to be the string pop_up
-            #radius= int(pdf.iloc[counter,1]),                      #masking problem
+
             radius=(1000 * pdf.iloc[counter,1]) / maxval,
             color= 'crimson',						# circle color
             fill= True,
             fill_color='crimson'					# fill color
         ).add_to(m)							# increment counter
         counter = 1 + counter
-        #print(pdf.iloc[counter,1])
+ 
     name = 'Q3-'+str(q3count)+'.html'					# name the file to whateever the count of q3count is
     m.save(name)							# save the map
     return q3count							# return to main
